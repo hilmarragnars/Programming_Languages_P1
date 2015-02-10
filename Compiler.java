ea97afcacdf0d14734f7d15d538910c3324cf81b
hilmarragnars <hilmarragnars@gmail.com>
2015-02-10 22:19:29 +0000

    added java-library

--- Compiler.java
+++ Compiler.java
@@ -1,9 +1,12 @@
+import edu.princeton.cs.introcs.*;
+
 public class Compiler{
 
 	public static void main(String args[]){
 		Lexer myLexer = new Lexer();
 		Parser myParser = new Parser(myLexer);
 		int k = myParser.parse();
-		System.out.println(k);	
+		StdOut.println(k);
+		StdOut.println("hellllllooooo");
 	}
 }
