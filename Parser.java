02c8bff15e3179b6ef697347dbf743704c79959e
hilmarragnars <hilmarragnars@gmail.com>
2015-02-12 00:29:30 +0000

    cahnges made in parser

--- Parser.java
+++ Parser.java
@@ -10,6 +10,21 @@ public class Parser{
 		currToken = this.lexer.nextToken();
 	}
 
+	private void statements(){
+		//do stuff
+	}
+	private void statement(){
+		//do stuff
+	}
+	private void expr(){
+		//do stuff
+	}
+	private void term(){
+		//do stuff
+	}
+	private void factor(){
+		//do stuff
+	}
 	public void parse(){
 		//do stuff
 		return;
